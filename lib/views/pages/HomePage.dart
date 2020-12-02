fe6dc8827d6fd1588ffdd3ab4b4783dd4b87d3d0
Alfian <alfian4299@gmail.com>
2020-12-02 13:59:18 +0700

    Touchup Homepage

--- lib/views/pages/HomePage.dart
+++ lib/views/pages/HomePage.dart
@@ -8,44 +8,117 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('SpeakUp - People Stories Social Media')
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              child: Text('Login'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => LoginPage())
-                );
-              }
-            ),
-            ElevatedButton(
-              child: Text('Register'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => RegisterPage())
-                );
-              }
-            ),
-            ElevatedButton(
-              child: Text('About'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => AboutPage())
-                );
-              }
-            )
-          ],
-        )
-      )
-    );
+    return Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('images/background.png'), fit: BoxFit.cover)),
+        child: Scaffold(
+            appBar:
+                AppBar(title: Text('SpeakUp - Share Stories From Around The World')),
+            body: Center(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+
+                Padding(
+                  padding: EdgeInsets.only(bottom: 40.0),
+                  child: Container(
+                    width: 200,
+                    height: 200,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('images/logo-primary-croped.PNG'),
+                        fit: BoxFit.cover
+                      )
+                    ),
+                  )
+                ),
+
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                        child: Text('Login'),
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => LoginPage()));
+                        }),
+                  ),
+                ),
+
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                        child: Text('Register'),
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => RegisterPage()));
+                        }),
+                  ),
+                ),
+
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                        child: Text('About'),
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => AboutPage()));
+                        }),
+                  ),
+                ),
+              ],
+            ))));
+
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     title: Text('SpeakUp - People Stories Social Media')
+    //   ),
+    //   body: Center(
+    //     child: Column(
+    //       mainAxisAlignment: MainAxisAlignment.center,
+    //       children: <Widget>[
+    //         ElevatedButton(
+    //           child: Text('Login'),
+    //           onPressed: () {
+    //             Navigator.push(
+    //               context,
+    //               MaterialPageRoute(builder: (context) => LoginPage())
+    //             );
+    //           }
+    //         ),
+    //         ElevatedButton(
+    //           child: Text('Register'),
+    //           onPressed: () {
+    //             Navigator.push(
+    //               context,
+    //               MaterialPageRoute(builder: (context) => RegisterPage())
+    //             );
+    //           }
+    //         ),
+    //         ElevatedButton(
+    //           child: Text('About'),
+    //           onPressed: () {
+    //             Navigator.push(
+    //               context,
+    //               MaterialPageRoute(builder: (context) => AboutPage())
+    //             );
+    //           }
+    //         )
+    //       ],
+    //     )
+    //   )
+    // );
   }
 }
