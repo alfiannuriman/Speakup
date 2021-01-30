44e47e3703013e900ece06aad3abab102bedabf8
unknown <dens96syah@gmail.com>
2021-01-30 14:18:54 +0700

    add searchpage

---
+++ lib/views/pages/SearchPage.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:speakup/views/pages/TimelinePage.dart';
+
+class SearchPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Search Page"),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icon.search),
+              onPressed: () {
+                showSearch(context: context, delegate: DataSearch());
+              })
+        ],
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
+
+class DataSearch extends SearchDelegate<String> {
+  final cities = [
+    "dena",
+    "joko",
+    "alfian",
+    "firman",
+  ];
+
+  final recentCities = [
+    "dena",
+    "joka",
+  ];
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: Icon(Icon.clear), 
+        onPressed: () {
+          query = "";
+       })
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+        icon: AnimatedIcon(
+          icon: AnimatedIcons.menu_arrow,
+          progress: transitionAnimation,
+        ),
+        onPressed: () {
+          close(context, null);
+        });
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // TODO: implement buildResults
+    return Center(
+      child: Container(
+        height: 100.0,
+        width: 100.0,
+        child: Card(
+          color: Colors.red,
+          child: Center(
+            child: Text(query),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // TODO: implement buildSuggestions
+    final suggestionList = query.isEmpty
+        ? recentCities
+        : cities.where((p) => p.startsWith(query)).toList();
+
+    return ListView.builder(
+      itemBuilder: (context, index) => ListTile(
+        onTap: (){
+          showResults(context)
+        },
+        leading: Icon(Icons.location_city),
+        title: RichText(
+          text: TextSpan(
+            text: suggestionList[index].substring(0, query.length),
+            style: TextStyle(
+              color: Colors.green, fontWeight: FontWeight,bold),
+            children: [
+              TextSpan(
+                text: suggestionList[index].substring(query.length),
+                style: TextStyle(color: Colors.grey))
+            ]),
+          ),
+        ),
+      itemCount: suggestionList.length,
+      ),
+    );
+  }
+}
